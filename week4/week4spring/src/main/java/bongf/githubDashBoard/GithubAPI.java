1346a45f647e39c3c4ad65b76b8374b42b16c4c3
bong6981 <kbk5190@naver.com>
2021-08-05 19:05:57 +0900

    Complete 과제1
    - live-study 대시 보드 만드는 코드 작성 완료

---
+++ week4/week4spring/src/main/java/bongf/githubDashBoard/GithubAPI.java
@@ -0,0 +1,73 @@
+package bongf.githubDashBoard;
+
+import org.kohsuke.github.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GithubAPI {
+    private final GitHub gitHub;
+    private final GHRepository repository;
+
+    public GithubAPI(String accessToken, String repoName) {
+        this.gitHub = connect(accessToken);
+        this.repository = getRepo(repoName);
+    }
+
+    private GitHub connect(String accessToken) {
+        try {
+            return new GitHubBuilder().withOAuthToken(accessToken)
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private GHRepository getRepo(String repoName) {
+        try {
+            return gitHub.getRepository(repoName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Set<String> getCommenterBy(int issueNum) {
+        Set<String> commenter = new HashSet<>();
+        List<GHIssueComment> comments = getComments(issueNum);
+
+        if (comments == null) {
+            return null;
+        }
+
+        for (GHIssueComment comment : comments) {
+            try {
+                commenter.add(comment.getUser().getLogin());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return commenter;
+    }
+
+    public Set<Integer> getIssueNumbers() {
+        PagedIterable<GHIssue> allGhIssues = repository.listIssues(GHIssueState.ALL);
+        Set<Integer> issueNumbers = new HashSet<>();
+        for (GHIssue ghIssue : allGhIssues) {
+            issueNumbers.add(ghIssue.getNumber());
+        }
+        return issueNumbers;
+    }
+
+    private List<GHIssueComment> getComments(int issueNum) {
+        try {
+            return repository.getIssue(issueNum).getComments();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ week4/week4spring/src/main/java/bongf/githubDashBoard/Main.java
@@ -0,0 +1,13 @@
+package bongf.githubDashBoard;
+
+public class Main {
+    private static final String accessToken = "";
+    private static final String repoName = "whiteship/live-study";
+
+    public static void main(String[] args) {
+        DashBoard dashBoard = new DashBoard(new GithubAPI(accessToken, repoName));
+        dashBoard.insertData();
+        dashBoard.showBoard();
+    }
+}
+
